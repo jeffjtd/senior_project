fa228c16f89770dfcb268a28f7b43a399b8daee5
jeffjtd <jeffjtd@ufl.edu>
2015-02-05 21:45:46 -0500

    FACEBOOK CHAT WORKING
    Currently showing most recent 5 chat messages, values can be changed to
    make it show more/less or only show unread messages

--- php/facebookApp.php
+++ php/facebookApp.php
@@ -1,74 +1,278 @@
 
-<?php
-  /* DECLARED VARIABLES */
- 
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php
     session_start();
- 
-	 require_once ('../fb/autoload.php');
+  ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    use Facebook\FacebookSession;
-    use Facebook\FacebookRedirectLoginHelper;
-    use Facebook\FacebookRequest;
-    use Facebook\FacebookResponse;
-    use Facebook\FacebookSDKException;
-    use Facebook\FacebookRequestException;
-    use Facebook\FacebookAuthorizationException;
-    use Facebook\GraphObject;
+    <title>Index</title>
 
-    // init app with app id (APPID) and secret (SECRET)
-    FacebookSession::setDefaultApplication('427760157376315', 'cafdf42e83e677212b2c90024789d231');
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../font-awesome/css/font-awesome.css" rel="stylesheet">
 
-    // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper( 'http://localhost:81/senior_project/php/facebookApp.php' );
+    <!-- Morris -->
+    <link href="../css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet">
 
-    try {
-      $session = $helper->getSessionFromRedirect();
-    } catch( FacebookRequestException $ex ) {
-      // When Facebook returns an error
-    } catch( Exception $ex ) {
-      // When validation fails or other local issues
-    }
+    <!-- Gritter -->
+    <link href="../js/plugins/gritter/jquery.gritter.css" rel="stylesheet">
 
-    // see if we have a session
-    if ( isset( $session ) ) {
-       // graph api request for user data
-      $request = new FacebookRequest( $session, 'GET', '/me/notifications' );
-      $response = $request->execute();
-      $graphObject = $response->getGraphObject()->asArray();    // get response
-      //displayNotifications($graphObject);
+    <link href="../css/animate.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
 
-      $request = new FacebookRequest($session, 'GET', '/me/inbox');
-      $response = $request->execute();
-      $graphObject = $response->getGraphObject()->asArray();
-      
-      displayMessages($graphObject);
-    } else {
-    	$params = array(
-    		'scope' => 'manage_notifications', 'read_mailbox'
-    		);
-      
-      echo '<a href="' . $helper->getLoginUrl($params) . '">Login</a>';   // show login url
-    }
+</head>
 
-    function displayNotifications($graphObject) {
-      for($i = 0; $i < sizeof($graphObject['data']); ++$i ) {
-        if($graphObject['data'][$i]->unread == 1){
-          echo '<pre>' . print_r( $graphObject['data'][$i]->title, 1);
-        }
-      }
-    }
+<body>
+    <div id="wrapper">
+        <nav class="navbar-default navbar-static-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="side-menu">
+                    <li class="nav-header">
+                        <div class="dropdown profile-element"> <span>
+                            <img alt="image" class="img-circle" src="../img/profile_small.jpg" />
+                             </span>
+                            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">Bilbo Baggins</strong>
+                             </span> <span class="text-muted text-xs block">Professional Burglar <b class="caret"></b></span> </span> </a>
+                            <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                                <li><a href="profile.html">Profile</a></li>
+                                <li><a href="contacts.html">Contacts</a></li>
+                                <li><a href="mailbox.html">Mailbox</a></li>
+                                <li class="divider"></li>
+                                <li><a href="login.html">Logout</a></li>
+                            </ul>
+                        </div>
+                        <div class="logo-element">
+                            IN+
+                        </div>
+                    </li>
+                    <li>
+                        <a href="../index.php"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboard</span></a>
+                    </li>
+                    <li>
+                        <a href="php/viewCalendar.php"><i class="fa fa-th-large"></i> <span class="nav-label">Google Calendar</span></a>
+                    </li>
+                    <li class="active">
+                        <a href="f#"><i class="fa fa-th-large"></i> <span class="nav-label">Facebook</span></a>
+                    </li>
+                </ul>
 
+            </div>
+        </nav>
 
-     function displayMessages($graphObject) {
-      //echo '<pre>' . print_r( $graphObject,1);
-      //echo '<pre>' . print_r( $graphObject['data'][0]->comments ,1);
-      
-      for($i = 0; $i < sizeof($graphObject['data']); $i++) {
-        for($k = 0; $k < sizeof($graphObject['data'][$i]->comments->data); $k++) {
-          if( isset($graphObject['data'][$i]->comments->data[$k]->message) ) {
-            echo '<pre>' . print_r( $graphObject['data'][$i]->comments->data[$k]->from->name, 1) . '</pre>';
-            echo '<pre>' . print_r( $graphObject['data'][$i]->comments->data[$k]->message, 1) . '</pre>';
-          }
-        }
-      }
-    }
+        <div id="page-wrapper" class="gray-bg dashbard-1">
+        <div class="row border-bottom">
+        <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+            <form role="search" class="navbar-form-custom" method="post" action="search_results.html">
+                <div class="form-group">
+                    <input type="text" placeholder="Search for something..." class="form-control" name="top-search" id="top-search">
+                </div>
+            </form>
+        </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li>
+                    <span class="m-r-sm text-muted welcome-message">Welcome to SMP.</span>
+                </li>
+                <li>
+                    <a href="login.html">
+                        <i class="fa fa-sign-out"></i> Log out
+                    </a>
+                </li>
+            </ul>
+
+        </nav>
+        </div>
+                <div class="row  border-bottom white-bg dashboard-header">
+            </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="wrapper wrapper-content">
+                                      <!--..............PHP................-->
+                                      <?php
+                                       require_once ('../fb/autoload.php');
+
+                                        use Facebook\FacebookSession;
+                                        use Facebook\FacebookRedirectLoginHelper;
+                                        use Facebook\FacebookRequest;
+                                        use Facebook\FacebookResponse;
+                                        use Facebook\FacebookSDKException;
+                                        use Facebook\FacebookRequestException;
+                                        use Facebook\FacebookAuthorizationException;
+                                        use Facebook\GraphObject;
+
+                                        // init app with app id (APPID) and secret (SECRET)
+                                        FacebookSession::setDefaultApplication('427760157376315', 'cafdf42e83e677212b2c90024789d231');
+
+                                        // login helper with redirect_uri
+                                        $helper = new FacebookRedirectLoginHelper( 'http://localhost:81/senior_project/php/facebookApp.php' );
+
+                                        try {
+                                          $session = $helper->getSessionFromRedirect();
+                                        } catch( FacebookRequestException $ex ) {
+                                          // When Facebook returns an error
+                                        } catch( Exception $ex ) {
+                                          // When validation fails or other local issues
+                                        }
+
+                                        // see if we have a session
+                                        if ( isset( $session ) ) {
+                                           // graph api request for user data
+                                          $request = new FacebookRequest( $session, 'GET', '/me/notifications' );
+                                          $response = $request->execute();
+                                          $graphObject = $response->getGraphObject()->asArray();    // get response
+                                          //displayNotifications($graphObject);
+
+                                          $request = new FacebookRequest($session, 'GET', '/me/inbox');
+                                          $response = $request->execute();
+                                          $graphObject = $response->getGraphObject()->asArray();
+                                          
+                                          displayMessages($graphObject);
+                                        } else {
+                                          $params = array(
+                                            'scope' => 'manage_notifications', 'read_mailbox'
+                                            );
+                                          
+                                          echo '<a href="' . $helper->getLoginUrl($params) . '">Login</a>';   // show login url
+                                        }
+
+                                        function displayNotifications($graphObject) {
+                                          for($i = 0; $i < sizeof($graphObject['data']); ++$i ) {
+                                            if($graphObject['data'][$i]->unread == 1){
+                                              echo '<pre>' . print_r( $graphObject['data'][$i]->title, 1);
+                                            }
+                                          }
+                                        }
+                                        function displayMessages($graphObject) {
+
+                                          $numChats = 0;
+
+                                          for($i = 0; $i < sizeof($graphObject['data']); $i++) {
+                                            $unreadMessages = $graphObject['data'][$i]->unread;
+                                            //Check to see if there are unread chats
+                                            if($unreadMessages >= 0) {
+
+                                              //Start a new row every 3 chats
+                                              if($numChats % 3 == 0)
+                                                 echo '<div class="row">';
+
+                                              //HTML header echo
+                                              echo '<div class="col-lg-4">
+                                                      <div class="ibox float-e-margins">
+                                                        <div class="ibox-title">';
+
+                                                            //Echo out users in group
+                                                            for($numUsers = 0; $numUsers < sizeof($graphObject['data'][$i]->to->data); $numUsers++)
+                                                              if( isset($graphObject['data'][$i]->to->data[$numUsers]) )
+                                                                if( $numUsers == sizeof($graphObject['data'][$i]->to->data) - 1 )
+                                                                  echo '<b>' . $graphObject['data'][$i]->to->data[$numUsers]->name . '</b>'; 
+                                                                else echo '<b>' . $graphObject['data'][$i]->to->data[$numUsers]->name . '</b>' . ', '; 
+
+                                                      //Echo the rest of the header
+                                                      echo '<div class="ibox-tools">
+                                                                <a class="collapse-link">
+                                                                    <i class="fa fa-chevron-up"></i>
+                                                                </a>
+                                                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                                                    <i class="fa fa-wrench"></i>
+                                                                </a>
+                                                                <ul class="dropdown-menu dropdown-user">
+                                                                    <li><a href="#">Config option 1</a>
+                                                                    </li>
+                                                                    <li><a href="#">Config option 2</a>
+                                                                    </li>
+                                                                </ul>
+                                                                <a class="close-link">
+                                                                    <i class="fa fa-times"></i>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                        <div class="ibox-content">';
+
+                                              //Echo all the chat data
+                                              /*for($k = 0; $k < sizeof($graphObject['data'][$i]->comments->data); $k++)
+                                                if( isset($graphObject['data'][$i]->comments->data[$k]->message) ) {
+                                                  echo '<b>' . $graphObject['data'][$i]->comments->data[$k]->from->name . '</b><br />';
+                                                  echo $graphObject['data'][$i]->comments->data[$k]->message . '<br />';
+                                                }
+                                              */
+                                              //Echo the messages that haven't been read
+                                              for($k = sizeof($graphObject['data'][$i]->comments->data); $k > sizeof($graphObject['data'][$i]->comments->data) - $unreadMessages - 5; $k--)
+                                                if( isset($graphObject['data'][$i]->comments->data[$k-1]->message) ) {
+                                                  echo '<b>' . $graphObject['data'][$i]->comments->data[$k-1]->from->name . '</b><br />';
+                                                  echo $graphObject['data'][$i]->comments->data[$k-1]->message . '<br />';
+                                                }
+
+                                              echo '</div>
+                                                    </div>
+                                                    </div>';
+                                              //End current row if this was the third chat
+                                              if( ($numChats + 1) % 3 == 0)
+                                                echo ' </div>';
+
+                                              $numChats++;
+                                            }
+                                          }
+                                        }
+                                    ?>
+                        </div>
+                </div>
+                <div class="footer">
+                    <div class="pull-right">
+                        10GB of <strong>250GB</strong> Free.
+                    </div>
+                    <div>
+                        <strong>Copyright</strong> Example Company &copy; 2014-2015
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+    <!-- Mainly scripts -->
+    <script src="../js/jquery-2.1.1.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="../js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Flot -->
+    <script src="../js/plugins/flot/jquery.flot.js"></script>
+    <script src="../js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+    <script src="../js/plugins/flot/jquery.flot.spline.js"></script>
+    <script src="../js/plugins/flot/jquery.flot.resize.js"></script>
+    <script src="../js/plugins/flot/jquery.flot.pie.js"></script>
+
+    <!-- Peity -->
+    <script src="../js/plugins/peity/jquery.peity.min.js"></script>
+    <script src="../js/demo/peity-demo.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="../js/inspinia.js"></script>
+    <script src="../js/plugins/pace/pace.min.js"></script>
+
+    <!-- jQuery UI -->
+    <script src="../js/plugins/jquery-ui/jquery-ui.min.js"></script>
+
+    <!-- GITTER -->
+    <script src="../js/plugins/gritter/jquery.gritter.min.js"></script>
+
+    <!-- EayPIE -->
+    <script src="../js/plugins/easypiechart/jquery.easypiechart.js"></script>
+
+    <!-- Sparkline -->
+    <script src="../js/plugins/sparkline/jquery.sparkline.min.js"></script>
+
+    <!-- Sparkline demo data  -->
+    <script src="../js/demo/sparkline-demo.js"></script>
+
+    <!-- ChartJS-->
+    <script src="../js/plugins/chartJs/Chart.min.js"></script>
+
+</body>
+</html>
